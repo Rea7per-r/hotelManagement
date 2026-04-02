58dc60493b2b21e71a596adbf05714732eedbf95
Rea7per-r <surajx070@gmail.com>
2026-04-02 20:44:15 +0530

    JWT & RBAC

--- src/main/java/com/suraj/hotelManagement/HotelManagementApplication.java
+++ src/main/java/com/suraj/hotelManagement/HotelManagementApplication.java
@@ -2,12 +2,21 @@ package com.suraj.hotelManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class HotelManagementApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(HotelManagementApplication.class, args);
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String raw = "Admin123";
+		String encoded = "$2a$10$te6aD/5iSxeaOEwsP.layO3pm9OAf3n08y4JYGel1cFl66M7ERcMa";
+
+		System.out.println(encoder.matches(raw, encoded));
 	}
 
 }
